47b8b23a431a79bce545f5ba5e9d6e1db5d01365
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-12-29 20:10:02 +0700

    s

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
+var email = document.querySelector('#email').value;
+var password = document.querySelector('#password').value;
+
 function login()
 {
-    var email = document.querySelector('#email').value;
-    var password = document.querySelector('#password').value;
-    console.log(email + "\n" + password)
+
 }
