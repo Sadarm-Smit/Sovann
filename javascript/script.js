6a4a31fcee2a3bb11fc0b0f59b76757fd3268142
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-12-29 20:31:09 +0700

    s

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+function login() {
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value;
+
+    if(email = "sadarm@gmail.com")
+    {
+        if(password = "root@117")
+        {
+            window.open("../page/home.html"); 
+        }
+        
+    }
+}
+
+function command(cmd) {
+    switch (cmd.toLowerCase()) {
+        case 'help':
+            help;
+            break;
+            case 'help':
+    }
+}
