ab7ca6e0879f5ba21b62cef25181321ac5b97e8e
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-12-29 19:41:28 +0700

    s

--- php/index.php
+++ php/index.php
@@ -1,41 +1,43 @@
 <?php
 $email = $_POST["email"];
 $passwd = $_POST["password"];
+echo "$email"
+// if (!empty($email) || !empty($passwd)) {
+//     $host = "localhost";
+//     $dbUsername = "root";
+//     $dbname = "sovann"
 
-if (!empty($email) || !empty($passwd)) {
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbname = "sovann"
+//     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+//     if(mysqli_connect_error()) {
+//         die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
+//     } else {
+//         $SELECT = "SELECT email From register Where email = ? Limit 1";
+//         $INSERT = "INSERT Into register (email, password) values (?,?)";
 
-    if(mysqli_connect_error()) {
-        die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
-    } else {
-        $SELECT = "SELECT email From register Where email = ? Limit 1";
-        $INSERT = "INSERT Into register (email, password) values (?,?)";
+//         $stmt = $conn->prepare($SELECT),
+//         $stmt->bind_param("s", $email);
+//         $stmt->execute()
+//         $stmt->bind_result($email);
+//         $stmt->store_result();
+//         $rnum = $stmt->num_rows;
+//         if(rnum==0) {
+//             $stmt->close();
 
-        $stmt = $conn->prepare($SELECT),
-        $stmt->bind_param("s", $email);
-        $stmt->execute()
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
-        if(rnum==0) {
-            $stmt->close();
+//             $stmt = $conn->prepare($INSERT);
+//             $stmt->bind_param("si",$email,$passwd);
+//             $stmt->execute();
+//             echo "New record insterted sucessfully";
+//         } echo {
+//             echo "Someone already register using this email";
+//         }
+//         $stmt->close();
+//         $conn->close();
+//     }
+// } else {
+//     echo "All feld are required";
+//     die();
+// }
 
-            $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("si",$email,$passwd);
-            $stmt->execute();
-            echo "New record insterted sucessfully";
-        } echo {
-            echo "Someone already register using this email";
-        }
-        $stmt->close();
-        $conn->close();
-    }
-} else {
-    echo "All feld are required";
-    die();
-}
+
 ?>
