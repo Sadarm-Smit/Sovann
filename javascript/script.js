778ee6db0d66348592421c2818f97af3c2032db9
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-12-29 20:05:18 +0700

    s

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,6 @@
 function login()
 {
-    console.log("LOGINING");
-    var input = document.querySelector('#input');
     var email = document.querySelector('#email').value;
     var password = document.querySelector('#password').value;
-    
-    var options="email = " + email + "\npassword = " + password;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("POST", "../php/index.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onload = function() {
-        console.log(xhr.responseText);
-        
-        input.innerHTML=xhr.responseText;
-    }
-    xhr.send(options);
+    console.log(email + "\n" + password)
 }
